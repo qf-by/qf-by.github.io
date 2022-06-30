2bbe2f723a9cab2cbc44d26f90e0425008f08333
qhby <353717996@qq.com>
2022-06-30 21:00:23 +0800

    Site updated: 2022-06-30 21:00:22

--- js/search.js
+++ js/search.js
@@ -1,80 +1,151 @@
-// 参考 https://chaooo.github.io/article/20161109.html
-var searchData;
-function loadData(arg) {
-    if (!searchData) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', '/content.json', true);
-        xhr.onload = function() {
-            if (this.status >= 200 && this.status < 300) {
-                var res = JSON.parse(this.response || this.responseText);
-                searchData = res instanceof Array ? res : res.posts;
-                searchkey(arg);
-            } else {
-                console.error(statusText);
-            }
-        };
-        xhr.onerror = function() {
-            console.error(statusText);
-        };
-        xhr.send();
-    } else {
-        searchkey(arg);
-    }
-}
+(function () {
+  var searchWord = document.getElementById("search-key"),
+    searchLocal = document.getElementById("search-local"),
+    searchForm = document.getElementById("search-form"),
+    searchMask = document.getElementById("result-mask"),
+    searchWrap = document.getElementById("result-wrap"),
+    searchResult = document.getElementById("search-result"),
+    searchTpl = document.getElementById("search-tpl").innerHTML,
+    winWidth,
+    winHeight,
+    searchData;
+  if (window.innerWidth) {
+    winWidth = parseInt(window.innerWidth);
+  } else if (document.body && document.body.clientWidth) {
+    winWidth = parseInt(document.body.clientWidth);
+  }
+  if (window.innerHeight) {
+    winHeight = parseInt(window.innerHeight);
+  } else if (document.body && document.body.clientHeight) {
+    winHeight = parseInt(document.body.clientHeight);
+  }
+  searchMask.style.width = winWidth + "px";
+  searchMask.style.height = winHeight + "px";
 
-function searchkey(keyword) {
-    keyword.forEach(word => {
-        searchData.forEach(function(post){
-            let rend = {};
-            let reg = new RegExp(word, 'g');
-            let flag = false;
-            if (post.title.search(reg) !== -1) {
-                rend.title = post.title.replace(reg, `<span class="keyword">${word}</span>`);
-                flag = true;
-            }
-            let textpos = post.text.search(reg);
-            if (textpos !== -1) {
-                rend.text = `…${post.text.substring(textpos, textpos+18)}…`;
-                rend.text = rend.text.replace(reg, `<span class="keyword">${word}</span>`);
-                flag = true;
-            }
-            if (flag) {
-                rend.title = !rend.title ? post.title : rend.title;
-                rend.text= !rend.text ? post.text : rend.text;
-                rend.href = `/${post.path}`;
-                render(rend);
-            }
-        });
+  function tpl(html, data) {
+    return html.replace(/\{\w+\}/g, function (str) {
+      var prop = str.replace(/\{|\}/g, "");
+      return data[prop] || "";
     });
-}
+  }
 
-function render(data) {
-    let ele = document.createElement('div');
-    ele.className = 'search-result';
-    ele.innerHTML = `<a href=${data.href}><h3>${data.title}</h3>
-    <span class="content">${data.text}<br>　</span></a>`;
-    document.getElementsByClassName('search-body')[0].appendChild(ele);
-}
+  function hasClass(obj, cls) {
+    return obj.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
+  }
 
-// 主程序
-let key = decodeURI(location.search.split('?q=')[1]);
-if (key !== undefined && key !== 'undefined') {
-    document.getElementsByClassName('search-input')[0].value = key;
-    loadData(format(key));
-    document.getElementById('nexmoe-search-space').style.display = 'flex';
-}
+  function addClass(obj, cls) {
+    if (!hasClass(obj, cls)) obj.className += " " + cls;
+  }
 
-// 事件
-function sclose() {
-    document.getElementById('nexmoe-search-space').style.display = 'none';
-}
+  function removeClass(obj, cls) {
+    if (hasClass(obj, cls)) {
+      var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+      obj.className = obj.className.replace(reg, " ");
+    }
+  }
 
-function sinput() {
-    document.getElementsByClassName('search-body')[0].innerHTML = '';
-    loadData(format(document.getElementsByClassName('search-input')[0].value));
-}
+  function matcher(post, regExp) {
+    return regtest(post.title, regExp) || regtest(post.text, regExp);
+  }
 
-// 搜索词格式化
-function format(word) {
-    return word.replace(/[ ]/g, '').split(' ');
-}
+  function regtest(raw, regExp) {
+    regExp.lastIndex = 0;
+    return regExp.test(raw);
+  }
+
+  function searchShow() {
+    removeClass(searchWrap, "hide");
+    removeClass(searchMask, "hide");
+  }
+
+  function searchHide() {
+    addClass(searchWrap, "hide");
+    addClass(searchMask, "hide");
+  }
+
+  function loadData(success) {
+    if (!searchData) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(
+        "GET",
+        document.getElementsByTagName("meta")["root"].content + "content.json",
+        true
+      );
+      xhr.onload = function () {
+        if (this.status >= 200 && this.status < 300) {
+          var res = JSON.parse(this.response || this.responseText);
+          searchData = res instanceof Array ? res : res.posts;
+          success(searchData);
+        } else {
+          console.error(this.statusText);
+        }
+      };
+      xhr.onerror = function () {
+        console.error(this.statusText);
+      };
+      xhr.send();
+    } else {
+      success(searchData);
+    }
+  }
+
+  function render(data) {
+    var html = "";
+    if (data.length) {
+      html = data
+        .map(function (post) {
+          return tpl(searchTpl, {
+            title: filter(post.title, "title"),
+            path: post.path,
+            content: filter(post.text, "content"),
+          });
+        })
+        .join("");
+    } else {
+      if (searchWord.value == "") {
+        searchHide();
+      } else {
+        html = '<div class="tips"><p>没有找到相关结果!</p></div>';
+      }
+    }
+    searchResult.innerHTML = html;
+  }
+
+  function filter(art, type) {
+    var keyword = searchWord.value;
+    var index = art.indexOf(keyword);
+    var artRe = art.replace(keyword, "<b>" + keyword + "</b>");
+    if (type == "title") {
+      return artRe;
+    }
+    if (type == "content" && index > 0) {
+      return artRe.substr(index - 15, 45);
+    }
+  }
+
+  function search(e) {
+    var key = this.value.trim();
+    if (!key) {
+      render("");
+      return;
+    }
+    var regExp = new RegExp(key.replace(/[ ]/g, "|"), "gmi");
+    loadData(function (data) {
+      var result = data.filter(function (post) {
+        return matcher(post, regExp);
+      });
+      render(result);
+    });
+    e.preventDefault();
+    searchShow();
+    searchWord.onfocus = function () {
+      searchShow();
+    };
+  }
+  searchWord.onfocus = function () {
+    searchWord.addEventListener("input", search);
+  };
+  searchMask.onclick = function () {
+    searchHide();
+  };
+})();
